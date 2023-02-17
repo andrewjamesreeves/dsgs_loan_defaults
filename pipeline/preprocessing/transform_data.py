a18d657bf8eabbeb535bd8dfd632406d19e1bdeb
reevea2 <andrew.reeves@ons.gov.uk>
2023-02-17 11:00:26 +0000

    additional functions in preprocessing to clean data

--- pipeline/preprocessing/transform_data.py
+++ pipeline/preprocessing/transform_data.py
@@ -1,4 +1,9 @@
+import data_utils as du
+from pandas.api.types import is_string_dtype
+
 import pandas as pd
+import numpy as np
+import os
 
 def format_column_names(df, ref):
 
@@ -27,12 +32,23 @@ def convert_binary_string_to_numeric(df, ref):
     
     return df
 
+def convert_string_elements(df):
 
-def main(df, data_ref):
+    for col in df:
+        if is_string_dtype(df[col]) == True:
+            df[col] = pd.to_numeric(df[col].str.replace('\D', ''))
+
+    return df
+
+
+def main(df, data_ref, paths):
 
     df = (df.pipe(format_column_names, data_ref)
         .pipe(split_categorical_variables, data_ref)
-        .pipe(convert_binary_string_to_numeric, data_ref))
+        .pipe(convert_categorical_to_numeric, data_ref)
+        .pipe(convert_binary_string_to_numeric, data_ref)
+        .pipe(convert_string_elements))
     
+    du.save_data(df, os.path.join(os.getcwd(), paths['filepaths']['preprocessed_data'], 'preprocessed_data.csv'))
 
     return
