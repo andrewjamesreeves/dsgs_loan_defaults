6ffb75115b077a8bb3ac140aa5a2b1a98bbd77b3
Reeves <Andrew.Reeves@ons.gov.uk>
2023-02-24 10:46:10 +0000

    added folders for models

---
+++ pipeline/models/model_control.py
@@ -0,0 +1,17 @@
+from models.ols import apply_ols
+from models.lasso import apply_lasso
+
+def get_available_models():
+    return
+
+def run_models():
+    return
+
+def main(training_data_pp, reference_data, models_config, paths, dir_name):
+
+    get_available_models()
+
+    run_models()
+
+
+    return
