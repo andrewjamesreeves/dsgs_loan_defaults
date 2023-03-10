1c06140f67034d449d6777e32e9644c7a78fa39e
Bui <Melissa.Bui@ons.gov.uk>
2023-03-10 08:52:44 +0000

    switched from statsmodel to sklearn package for analysis

--- pipeline/models/logit.py
+++ pipeline/models/logit.py
@@ -1,25 +1,45 @@
-import statsmodels.api as sm
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+
+
 
 def apply_logit(training_data, test_data, config):
 
+    # Split data
     Y_train = training_data[['loan_status']]
-    X_train = training_data.drop(['loan_status'], axis=1).iloc[:,0:33]
-    X_train = sm.add_constant(X_train)
+    X_train = training_data.drop(['loan_status'], axis=1)
 
     Y_test = test_data[['loan_status']]
-    X_test = test_data.drop(['loan_status'], axis=1).iloc[:,0:33]
-    X_test = sm.add_constant(X_test)
-
+    X_test = test_data.drop(['loan_status'], axis=1)
     
-    log_regression = sm.Logit(Y_train,X_train)
-    results = log_regression.fit()
+    # Define parameters  
+    logistic_regression_model = LogisticRegression(solver=config['solver'],  
+                                        C=config['C'], 
+                                        random_state=config['random_state'], 
+                                        class_weight=config['class_weight'])
+            
+    # Fit the model
+    logistic_regression_model.fit(X_train, Y_train)
 
-    yhat = round(results.predict(X_test)).to_frame(name='y_pred').sort_index()
-
-    error_df = Y_test.merge(yhat, left_index=True, right_index=True)
+    # Predict values using the test data.
+    yhat = logistic_regression_model.predict(X_test)
+    
+    # Create dataframe with predicted and actual values    
+    yhat_df = pd.DataFrame(yhat, columns = ['y_pred']).sort_index()
+    
+    error_df = Y_test.merge(yhat_df, left_index=True, right_index=True)
     error_df['correct'] = error_df.y_pred == error_df.loan_status
 
-    error = len(error_df[error_df.correct == True])/len(error_df)
+    # Produce evaluation metrics
+    target_names = ["No default", "Default"] # Set the names for our report to produce.
 
-    return
+    # Generate the report using the target test and prediction values.
+    classif_report = classification_report(Y_test, yhat, target_names=target_names)
+    
+    print(classif_report)
+    
+    return 
+
+
